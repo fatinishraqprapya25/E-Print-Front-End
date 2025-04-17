f1840a6c10ddbc9ae325217418c5a06bd71bc61e
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-04-17 15:43:04 +0600

    service header done

---
+++ src/components/home/Services.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Services() {
+    return <>
+        <section className="mt-20">
+            <div className="text-center w-full mx-auto md:w-[500px]">
+                <h1 className="font-medium text-black text-3xl">Our Services</h1>
+                <p className="text-gray-600 mt-3 line-clamp-2">We offer a wide range of printing and finishing services to meet all your requirements.</p>
+            </div>
+        </section>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+    </>
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/common/header";
 import Hero from "../components/home/Hero";
+import Services from "../components/home/Services";
 
 export default function Home() {
     return <>
         <Header />
         <Hero />
+        <Services />
     </>
 }
